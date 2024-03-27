ff84830ab1d8dfcc94a7e63f9c19b75b93ef5770
patriciamv97 <72247871+patriciamv97@users.noreply.github.com>
2024-03-27 19:50:36 +0000

    union tablas y conexion

--- src/app.py
+++ src/app.py
@@ -1,4 +1,26 @@
 from utils import db_connect
+import pandas as pd
+
+
 engine = db_connect()
 
-# your code here
+engine
+if engine:
+    print("Conexión exitosa")
+
+data = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/k-nearest-neighbors-project-tutorial/main/tmdb_5000_movies.csv')
+data.to_sql('tabla_peliculas', engine, if_exists='replace', index=False)
+
+data2 = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/k-nearest-neighbors-project-tutorial/main/tmdb_5000_credits.csv')
+data2.to_sql('tabla_creditos', engine, if_exists='replace', index=False)
+
+# Unir las dos tablas por el título
+query = """
+SELECT *
+FROM tabla_peliculas AS peliculas
+JOIN tabla_creditos AS creditos
+ON peliculas.title = creditos.title
+"""
+
+
+
